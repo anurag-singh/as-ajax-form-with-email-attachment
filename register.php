f16b8e1d9060e28a2f2281e6ee282cb301e5bb8b
Anurag Singh <contact@anuragsingh.me>
2017-09-06 10:32:23 +0530

    initial setup

---
+++ register.php
@@ -0,0 +1,434 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		
+		<!-- Stylesheets
+		============================================= -->
+		<link href="https://fonts.googleapis.com/css?family=Cookie|Open+Sans:400,600,700,800,900|Poppins:300,400,500,600,700|Playfair+Display:400,400italic,700,700italic,900" rel="stylesheet">
+		<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+		<link rel="stylesheet" href="style.css" type="text/css" />
+		<link rel="stylesheet" href="css/dark.css" type="text/css" />
+		<link rel="stylesheet" href="css/swiper.css" type="text/css" />
+		
+		<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+		
+		<link rel="stylesheet" href="css/animate.css" type="text/css" />
+		<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+		<!-- Bootstrap File Upload CSS -->
+		<link rel="stylesheet" href="css/components/bs-filestyle.css" type="text/css" />
+		<link rel="stylesheet" href="css/responsive.css" type="text/css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+		<![endif]-->
+		<link rel="stylesheet" href="css/colors.php?color=54b2f4" type="text/css" />
+		
+		<link href="royalslider/royalslider.css" rel="stylesheet">
+		<link href="royalslider/skins/minimal-white/rs-minimal-white.css" rel="stylesheet">
+		<!-- Document Title
+		============================================= -->
+		<title>Senior Assist</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	</head>
+	<body class="stretched" >
+		<!-- Document Wrapper
+		============================================= -->
+		<div id="wrapper" class="clearfix">
+			<!-- Header
+			============================================= -->
+			<header id="header" class="header sticky split-menu clearfix">
+				<div id="header-wrap">
+					<div class="container clearfix">
+						<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+						<!-- Logo
+						============================================= -->
+						<div id="logo">
+							<a href="index.html" class="standard-logo"><img src="images/senior-assist-logo.png" alt="Senior Assist"></a>
+							<a href="index.html" class="retina-logo"><img src="images/senior-assist-logo.png" alt="Senior Assist"></a>
+							</div><!-- #logo end -->
+							<!-- Primary Navigation
+							============================================= -->
+							<nav id="primary-menu" class="with-arrows clearfix">
+								<ul>
+									<li><a href="about-us.html"><div>ABOUT US</div></a></li>
+									<li><a href="security.html"><div>SECURITY</div></a></li>
+									<li><a href="services.html"><div>SERVICES</div></a></li>
+									<li><a href="be-a-caregiver.html"><div>Be A Caregiver</div></a></li>
+								</ul>
+								<ul>
+									<li><a href="register.html" class="color"><div>REGISTER</div></a></li>
+									<li><a href="pay-here.html"><div>Pay Here</div></a></li>
+									<li><a href="testimonials.html"><div>TESTIMONIALS</div></a></li>
+									<li><a href="contact-us.html"><div>CONTACT US</div></a></li>
+									<!--<li><a href="index.html" class="color"><div>HOME</div></a></li>-->
+								</ul>
+								</nav><!-- #primary-menu end -->
+							</div>
+						</div>
+						</header><!-- #header end -->
+						
+						<!-- Slider
+						============================================= -->
+						<section id="slider" class="slider" style="background-color: #222;">
+							<!--<div id="full-width-slider" class="royalSlider heroSlider rsMinW">
+									<div class="rsContent">
+											<img class="rsImg" src="images/servcies.jpg" alt="Senior Assist">
+									</div>
+									
+							</div>-->
+						</section>
+						
+						<!-- Content
+						============================================= -->
+						<!--<section id="content" style="overflow: visible;">
+									<div style="position: absolute; background-color:#ceebf4; top: 0; left: 0; width: 100%; z-index: 3;  height: 8px; margin-top: -8px;"></div>
+								
+							</section>--><!-- #content end -->
+							
+							<div class="container clearfix topmargin bottommargin">
+								
+								<div class="saheading notopmargin">
+									Registration form for Care Recipient
+								</div>
+								<div class="center" style="font-size:12px; margin-top:10px; color: #b3b3b3;">
+									Fill online or Download PDF. Fields marked * are mandatory.
+								</div>
+								
+								<div class="sabodytext topmargin">
+									
+									<div class="col_two_third aligncenter">
+										<div class="contact-widget">
+											<div class="contact-form-result"></div>
+												<?php include "include/form-register.php"  ?>	
+											</div>
+										</div>
+										<!-- Form -->
+										
+									</div>
+									
+									<img class=" aligncenter"  src="images/sa-bird.jpg" alt="Senior Assist" >
+									
+								</div>
+								
+								<!-- 3 column -->
+								
+								<div class="col-md-12 nopadding common-height">
+									
+									<div class="col_one_third" >
+										<div class="entry-image">
+											<a href="be-a-caregiver.html"><img  src="images/servcies-left.jpg" alt="Senior Assist" >
+												<div class="overlay">
+													<div class="text-overlayin2" >
+														<div class="overlay-wrap">
+															<div class="sa-box-l" >
+																
+																<div><h11>BE A CAREGIVER</h11></div>
+																
+																<div>
+																	<h13>Read More</h13>
+																</div>
+																
+															</div>
+														</div></div>
+													</div></a>
+												</div></div>
+												
+												<div class="col_one_third" >
+													<div class="entry-image">
+														<img  src="images/center.jpg" alt="Senior Assist" >
+														<!--<div class="overlay">
+																
+																<div class="text-overlayin2">
+																		<div class=" sa-box-l">
+																				
+																						<div class="text-overlay-title" style="text-align:center;">
+																									<h14>LIFE WITHOUT</h14><br>
+																						<h15>COMPROMISE</h15>
+																						</div>
+																						
+																				</div>
+																	</div>
+																
+														</div>-->
+													</div></div>
+													
+													<div class="col_one_third col_last">
+														<div class="entry-image">
+															<a href="security.html"><img  src="images/servcies-right.jpg" alt="Senior Assist" >
+																<div class="overlay">
+																	<div class="text-overlayin2" >
+																		<div class="overlay-wrap">
+																			<div class="sa-box-l" >
+																				
+																				<div><h11>SECURITY</h11></div>
+																				
+																				<div>
+																					<h13>Read More</h13>
+																				</div>
+																				
+																			</div>
+																		</div></div>
+																	</div></a>
+																</div>
+															</div>
+															
+														</div>
+														
+														<!-- 3 column -->
+														
+														<div class="clear"></div>
+														
+														<!-- Footer
+														============================================= -->
+														<footer id="footer" class="dark">
+															
+															<div class="container">
+																<!-- Footer Widgets
+																============================================= -->
+																<div class="footer-widgets-wrap clearfix">
+																	<div class="col_one_third2">
+																		<div class="widget quick-contact-widget clearfix">
+																			<h4>SENIOR ASSIST</h4>
+																			<p>Senior Assist offers an individualized approach to keep seniors safe and sound at home. It enhances the quality of life for both seniors and their families in every caregiving experience. </p>
+																			<div >
+																				<address>
+																					<strong>Registered Office Address:</strong><br>
+																					RZ 63/29,<br>
+																					Tuglakabad Extension, First Floor,<br>
+																					New Delhi - 110 019, India<br>
+																				</address>
+																			<abbr ><strong>Email:</strong></abbr> info@seniorassist.in<br>
+																		<abbr ><strong>Mobile:</strong></abbr> +91- 9810098100<br>
+																		
+																	</div>
+																</div>
+															</div>
+															<div class="col_one_third2">
+																<div class="widget clearfix">
+																	<h4>Client Testimonials</h4>
+																	<div class="fslider testimonial no-image nobg noborder noshadow nopadding" data-animation="slide" data-arrows="false">
+																		<div class="flexslider">
+																			<div class="slider-wrap">
+																				
+																				<div class="slide">
+																					
+																					<div class="testi-content">
+																						<p>Taking care of our father, Retd. Ambassador Gunwant Singh Malik, was done with great diligence, intelligence, sensitivity and good cheer. We would recommend the services without any reservations.</p>
+																						<div class="testi-meta">
+																							Kiran Malik
+																							
+																						</div>
+																					</div>
+																				</div>
+																				<div class="slide">
+																					
+																					<div class="testi-content">
+																						<p>A warm relationship was built with our father and a genuine interest was taken in his well-being. He was boosted by the care givers cheerful disposition and enjoyed conversing with her on a wide range of topics.</p>
+																						<div class="testi-meta">
+																							Arun Malik
+																							
+																						</div>
+																					</div>
+																				</div>
+																				
+																				<div class="slide">
+																					
+																					<div class="testi-content">
+																						<p>Exhibited tact and judgement in their interactions with our father, as well as with us and the multiple individuals involved in his care. She was liked by the people involved in our father's care and her conduct routinely elicited appreciative comments from them. Would highly recommend their services. </p>
+																						<div class="testi-meta">
+																							Ritu Singh
+																							
+																						</div>
+																					</div>
+																				</div>
+																				
+																				<div class="slide">
+																					
+																					<div class="testi-content">
+																						<p>The communication gap between our mother and us seemed to have vanished. We could monitor the situation with so much more confidence knowing that someone reliable and dependable was there when required. </p>
+																						<div class="testi-meta">
+																							Daniel Joseph
+																							
+																						</div>
+																					</div>
+																				</div>
+																				
+																				<div class="slide">
+																					
+																					<div class="testi-content">
+																						<p>Got the zest for life back in our father. He could do so many things which he had lost interest in, as he did not enjoy doing them alone. He even looked forward to going out for a cup of coffee and have an intellectually stimulating conversation with the cultured and well educated people sent by Senior Assist. </p>
+																						<div class="testi-meta">
+																							Md. Khalid Sayeed
+																							
+																						</div>
+																					</div>
+																				</div>
+																				
+																			</div>
+																		</div>
+																	</div>
+																</div>
+																
+															</div>
+															<div class="col_one_third2 col_last">
+																<div class="widget quick-contact-widget clearfix">
+																	<h4>Contact Us</h4>
+																	
+																	<p>
+																		We tailor our services to your unique situation -- and adjust as your needs evolve. We provide dependable, compassionate care at all levels, from companionship, medical requirements and daily chores, we have you covered. Senior Assist provides a total homecare solution.<br> <span><a href="contact-us.html">Click Here</a></span> to get in touch with us. </p>
+																		
+																	</div>
+																	</div><!-- .footer-widgets-wrap end -->
+																</div>
+																
+															</div>
+															<!-- Copyrights
+															============================================= -->
+															<div id="copyrights">
+																<div class="container clearfix">
+																	<div class="col_one_third nomargin">
+																		Copyrights © 2017 Senior Assist<br>
+																		
+																	</div>
+																	
+																	<div class="col_one_third nomargin">
+																		<div class="clearfix divcenter" style="width:120px;">
+																			
+																			<a href="#" class="social-icon si-small si-borderless nobottommargin si-gplus">
+																				<i class="icon-mail"></i>
+																				<i class="icon-mail"></i>
+																			</a>
+																			
+																			<a href="#" class="social-icon si-small si-borderless si-facebook">
+																				<i class="icon-facebook"></i>
+																				<i class="icon-facebook"></i>
+																			</a>
+																			<a href="#" class="social-icon si-small si-borderless si-youtube">
+																				<i class="icon-youtube"></i>
+																				<i class="icon-youtube"></i>
+																			</a>
+																			
+																		</div>
+																	</div>
+																	
+																	<div class="col_one_third col_last nomargin tright">
+																		
+																		<div class="copyright-links"><a href="terms-of-use.html">Terms of Use</a> / <a href="privacy-policy.html">Privacy Policy</a></div>
+																		
+																	</div>
+																</div>
+																</div><!-- #copyrights end -->
+																</footer><!-- #footer end -->
+																</div><!-- #wrapper end -->
+																<!-- Go To Top
+																============================================= -->
+																<div id="gotoTop" class="icon-line-arrow-up"></div>
+																<!-- External JavaScripts
+																============================================= -->
+																<script type="text/javascript" src="js/jquery.js"></script>
+
+																<script type="text/javascript" src="js/plugins.js"></script>
+																<!-- Footer Scripts
+																============================================= -->
+																<script type="text/javascript" src="js/functions.js"></script>
+																<!-- royal SLIDER SCRIPTS  -->
+																
+																<script src="royalslider/jquery.royalslider.min.js"></script>
+																<!-- Bootstrap File Upload Plugin -->
+																<script type="text/javascript" src="js/components/bs-filestyle.js"></script>
+																
+																<script>
+																jQuery(document).ready(function($) {
+																$('#full-width-slider').royalSlider({
+																arrowsNav: true,
+																loop: true,
+																keyboardNavEnabled: true,
+																controlsInside: false,
+																imageScaleMode: 'fill',
+																arrowsNavAutoHide: true,
+																autoScaleSlider: true,
+																autoScaleSliderWidth: 1200,
+																autoScaleSliderHeight: 588,
+																controlNavigation: 'bullets',
+																thumbsFitInViewport: false,
+																navigateByClick: true,
+																startSlideId: 0,
+																	autoPlay: true,
+																transitionType:'fade',
+																globalCaption: false,
+																deeplinking: {
+																enabled: true,
+																change: false
+																},
+																	autoPlay: {
+																enabled: true,
+																delay: 3000
+																},
+																	
+																/* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
+																imgWidth: 1200,
+																imgHeight: 588
+																});
+																});
+																</script>
+																<!-- 
+																<script  type="text/javascript">
+																		$(document).on('ready', function() {
+																			$("#input-5").fileinput({showCaption: false});
+																			$("#input-6").fileinput({
+																				showUpload: false,
+																				maxFileCount: 10,
+																				mainClass: "input-group-lg",
+																				showCaption: true
+																			});
+																			$("#input-8").fileinput({
+																				mainClass: "input-group-md",
+																				showUpload: true,
+																				previewFileType: "image",
+																				browseClass: "btn btn-success",
+																				browseLabel: "Pick Image",
+																				browseIcon: "<i class=\"icon-picture\"></i> ",
+																				removeClass: "btn btn-danger",
+																				removeLabel: "Delete",
+																				removeIcon: "<i class=\"icon-trash\"></i> ",
+																				uploadClass: "btn btn-info",
+																				uploadLabel: "Upload",
+																				uploadIcon: "<i class=\"icon-upload\"></i> "
+																			});
+																			$("#input-9").fileinput({
+																				previewFileType: "text",
+																				allowedFileExtensions: ["txt", "md", "ini", "text"],
+																				previewClass: "bg-warning",
+																				browseClass: "btn btn-primary",
+																				removeClass: "btn btn-default",
+																				uploadClass: "btn btn-default",
+																			});
+																			$("#input-10").fileinput({
+																				showUpload: false,
+																				layoutTemplates: {
+																					main1: "{preview}\n" +
+																					"<div class=\'input-group {class}\'>\n" +
+																							"   <div class=\'input-group-btn\'>\n" +
+																									"       {browse}\n" +
+																									"       {upload}\n" +
+																									"       {remove}\n" +
+																							"   </div>\n" +
+																							"   {caption}\n" +
+																					"</div>"
+																				}
+																			});
+																			$("#input-11").fileinput({
+																				maxFileCount: 10,
+																				allowedFileTypes: ["image", "video"]
+																			});
+																			$("#input-12").fileinput({
+																				showPreview: false,
+																				allowedFileExtensions: ["zip", "rar", "gz", "tgz"],
+																				elErrorContainer: "#errorBlock"
+																			});
+																		}); -->
+																</script>
+																<script type="text/javascript" src="js/as-ajax-email.js"></script>
+															</body>
+														</html>
