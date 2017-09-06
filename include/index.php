9dd69ddf36ed3a33f1edfcd33f044aea6096c6eb
Anurag Singh <contact@anuragsingh.me>
2017-09-06 17:59:28 +0530

    include form

---
+++ include/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Email along with attchment with AJAX</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+	<script src="include/js/form-processing.js"></script>
+</head>
+<body>
+	<?php include "include/form-register.php"  ?>
+</body>
+</html>
